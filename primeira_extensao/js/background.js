831f841f4f6cd184b81922a2457d235f5c2055cd
George Leandro <george.leandro@thomsonreuters.com>
2022-01-20 15:14:29 -0300

    CSS dinamico no popup, trocando mensagens entre o CSS e o background

--- primeira_extensao/js/background.js
+++ primeira_extensao/js/background.js
@@ -1,3 +1,4 @@
+//Function to execute when the extension be installed
 // chrome.runtime.onInstalled.addListener((reason) => {
 //   if (reason === chrome.runtime.OnInstalledReason.INSTALL) {
 //     chrome.tabs.create({
@@ -6,8 +7,20 @@
 //   }
 // });
 
-chrome.runtime.onMessage.addListener(function(response, sender,sendResponse){
-	alert(response);
-});
+
+
+//waiting a message
+chrome.runtime.onMessage.addListener(
+	//I added a callback function
+	(response, sender,sendResponse) => {
+		//I only execute this part if I receive this object 
+		if(response.rules !== null){
+			alert('Eu disse "'+response.rules+'"');
+
+			//Response that im sending 
+			sendResponse({resposta:"Eu também quero"})
+		}
+	}
+);
 
 //response = o que recebemos
--- primeira_extensao/js/popup.js
+++ primeira_extensao/js/popup.js
@@ -1,9 +1,34 @@
 $(function(){
+	var x= 0;
 	$("#name").keyup(function(){
+		//Incrementing to chang the color
+		x = x +100;
+		var color = x.toString(16);
+		color = color.padStart(6, "0");
+		
+
+		//Changing CSS
+		$("#meu").css("background-color", "#"+color);
+
+		//Changing HTML text
 		$("#greet").text("Hello "+ $("#name").val());
+
+
+		//Checking HTML element value
 		if($("#name").val() == "George"){
-			chrome.runtime.sendMessage("Faraó");
+			//Sending message
+			chrome.runtime.sendMessage(
+				//Im sending this object
+				{rules:"Quero biscoito"},
+
+				//I want to execute this function if I receive a response
+				function(response){
+					alert('E você respondeu: "'+response.resposta);
+					//alert(x);
+					//alert(color);
+			});
 		}
-		//alert("clicou");
+		
 	});
 })
+
