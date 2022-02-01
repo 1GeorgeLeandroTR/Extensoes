a95b881f9bb86e0d41ecd97d969f6b2e796fb0f0
George Leandro <george.leandro2@thomsonreuters.com>
2022-01-31 22:03:58 -0300

    correcao do erro de multiplos iframes e correcao no design

--- salvador_de_imagem/js/content_conf/content.js
+++ salvador_de_imagem/js/content_conf/content.js
@@ -3,28 +3,30 @@
     //talvez deixar tudo dentro da mesma função (){tudo}()\
 
 var visibilidade = false;
-
+var executed = false;
 function colocar(){
+    if(executed == false){
     var iframe = document.createElement('iframe');
     iframe.style.width = "16.25rem";
     iframe.style.background = "#eee";
     iframe.style.height = "17.5rem";
     iframe.style.position = "fixed";
     iframe.style.top = "0px";
-
-    iframe.id = "meu-frame";
+    iframe.style.border = "none";
     iframe.style.right = "0px";
-    iframe.style.zIndex = "1000000000000000";
-    /* end of settings */
+    iframe.style.zIndex = "99999999999999999";    
+    iframe.id = "meu-frame";
     iframe.src = chrome.runtime.getURL("../templates/popup.html");
     document.body.appendChild(iframe);
+    executed = true;
+    }
 };
 
-function apaga(){
+const apaga = ()=>{
       var iframe = document.getElementById("meu-frame");
       iframe.style.visibility = "hidden";
       iframe.parentNode.removeChild(iframe);
-
+      executed = false;
 };
 
 function mostra(){
